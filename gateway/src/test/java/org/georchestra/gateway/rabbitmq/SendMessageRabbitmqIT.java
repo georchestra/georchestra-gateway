ddef03f967bb0164de0a7bb225c22bac040d7351
Florian Necas <florian.necas@camptocamp.com>
2025-03-18 17:18:00 +0100

    wip: format

--- gateway/src/test/java/org/georchestra/gateway/rabbitmq/SendMessageRabbitmqIT.java
+++ gateway/src/test/java/org/georchestra/gateway/rabbitmq/SendMessageRabbitmqIT.java
@@ -84,8 +84,7 @@ public class SendMessageRabbitmqIT {
                 .withEnv("smtpPort", String.valueOf(smtp.getMappedPort(SMTPPORT))).withLogToStdOut();
 
         console.start();
-        System.setProperty("georchestra.console.url",
-                "http://localhost:%d".formatted(console.getMappedConsolePort()));
+        System.setProperty("georchestra.console.url", "http://localhost:%d".formatted(console.getMappedConsolePort()));
     }
 
     public static @AfterAll void shutDownContainers() {
