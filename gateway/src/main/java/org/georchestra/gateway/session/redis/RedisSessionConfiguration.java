44c8193c3253000fb21584626ef0047261e86a43
Florian Necas <florian.necas@camptocamp.com>
2026-04-16 13:48:42 +0200

    refactor: clean up RedisSessionConfiguration by removing unused imports

--- gateway/src/main/java/org/georchestra/gateway/session/redis/RedisSessionConfiguration.java
+++ gateway/src/main/java/org/georchestra/gateway/session/redis/RedisSessionConfiguration.java
@@ -18,20 +18,11 @@
  */
 package org.georchestra.gateway.session.redis;
 
-import java.time.Duration;
-
 import org.georchestra.gateway.security.ldap.extended.GeorchestraUserNamePasswordAuthenticationToken;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.data.redis.RedisReactiveHealthIndicator;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -40,14 +31,11 @@ import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
 import lombok.extern.slf4j.Slf4j;
 
 /**
